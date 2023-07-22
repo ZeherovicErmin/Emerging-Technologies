2c7e6c8d064ef051b2abc1042a25f85d05b9090b
Hasan <Hasanahmad4540@gmail.com>
2023-07-22 19:32:55 -0400

    initial shopping cart commit

---
+++ ShoppingCart.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify, request
+import json
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return "Hello World"
+
+def get_products_from_products_microservice(productName):
+    products_url = 'http://127.0.0.1:80/product?name=' + productName 
+    response = requests.get(products_url)
+
+    if response.status_code == 200:
+        products_data = response.json()
+        return products_data
+    else:
+        return None  # Handle the case where the request failed or returned an error
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10)
