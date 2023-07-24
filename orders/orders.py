c9167e118283a968e9c039b30d7e8d807d1aa01b
punithAJ-hub <punithaj0007@gmail.com>
2023-07-24 16:59:22 -0400

    endpoint to fetch order details

--- orders/orders.py
+++ orders/orders.py
@@ -33,57 +33,82 @@ class Orders:
         def getQuantity(self):
             return self.quantity
 
-        def displayOrders(orders):
-            order_info = []
 
-            if (type(orders) == list.__class__):
-                for order in orders:
-                    order_info.append({
-                        'orderID': order.getOrderID(),
-                        'productID': order.getProductID(),
-                        'userID': order.getUserID(),
-                        'quantity': order.getQuantity()
 
-                    })
-            else:
-                order_info.append({
-                    'orderID': orders.getOrderID(),
-                    'productID': orders.getProductID(),
-                    'userID': orders.getUserID(),
-                    'quantity': orders.getQuantity()
 
-                })
-            return order_info
+@app.route('/allOrders')
+def main():
+    orders = loadJsonAsList()
+    return jsonify(displayOrders(orders))
 
-        def load_json():
-            with open("./static/orders.json", "r") as file:
-                data = json.load(file)
-            return data
+@app.route('/orders/<orderID>', methods=['GET'])
+def findByOrderID(orderID):
+    orders = loadJsonAsList()
+    orderID = request.view_args['orderID']
+    print(orderID)
+    print(orders)
+    for order in orders:
+        print(order.getOrderID())
+        if(order.getOrderID()==orderID):
+            return displayOrders(order)
 
-        def save_json(data):
-            with open("./static/orders.json", "w") as file:
-                json.dump(data, file, indent=2)
+    return "No such orders found with  order ID :" + orderID
 
-        def loadJsonAsList():
-            # Read JSON file
-            with open('./static/orders.json', 'r') as file:
-                data = json.load(file)
 
-            # Access JSON file into array and create a list to host parsed info
-            ordersInfo = data['orders']
-            orders = []
+def displayOrders(orders):
+    order_info = []
 
-            # Iterate over json file into array
-            for orderdetail in ordersInfo:
-                order_id = orderdetail['orderID']
-                product_id = orderdetail['productID']
-                user_id = orderdetail['userID']
-                quantity = orderdetail['quantity']
+    if (type(orders) == list.__class__):
+        for order in orders:
+            order_info.append({
+                'orderID': order.getOrderID(),
+                'productID': order.getProductID(),
+                'userID': order.getUserID(),
+                'quantity': order.getQuantity()
 
-                # Create an instance of the Products class with the parsed information
-                order = Orders(order_id, product_id, user_id, quantity)
+            })
+    else:
+        order_info.append({
+            'orderID': orders.getOrderID(),
+            'productID': orders.getProductID(),
+            'userID': orders.getUserID(),
+            'quantity': orders.getQuantity()
 
-                # Add the order to the list of orders
-                orders.append(order)
+        })
+    return order_info
 
-            return orders
+
+def load_json():
+    with open("./static/orders.json", "r") as file:
+        data = json.load(file)
+    return data
+
+
+def save_json(data):
+    with open("./static/orders.json", "w") as file:
+        json.dump(data, file, indent=2)
+
+
+def loadJsonAsList():
+    # Read JSON file
+    with open('./static/orders.json', 'r') as file:
+        data = json.load(file)
+
+    # Access JSON file into array and create a list to host parsed info
+    ordersInfo = data['orders']
+    orders = []
+
+    # Iterate over json file into array
+    for orderdetail in ordersInfo:
+        order_id = orderdetail['orderID']
+        product_id = orderdetail['productID']
+        user_id = orderdetail['userID']
+        quantity = orderdetail['quantity']
+
+        # Create an instance of the Products class with the parsed information
+        order = Orders(order_id, product_id, user_id, quantity)
+
+        # Add the order to the list of orders
+        orders.append(order)
+
+    return orders
