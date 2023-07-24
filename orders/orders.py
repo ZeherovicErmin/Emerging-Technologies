ce24722d84223433217ffa614b3319574454f198
punithAJ-hub <punithaj0007@gmail.com>
2023-07-24 16:46:13 -0400

    method to load and save data

--- orders/orders.py
+++ orders/orders.py
@@ -54,3 +54,36 @@ class Orders:
 
                 })
             return order_info
+
+        def load_json():
+            with open("./static/orders.json", "r") as file:
+                data = json.load(file)
+            return data
+
+        def save_json(data):
+            with open("./static/orders.json", "w") as file:
+                json.dump(data, file, indent=2)
+
+        def loadJsonAsList():
+            # Read JSON file
+            with open('./static/orders.json', 'r') as file:
+                data = json.load(file)
+
+            # Access JSON file into array and create a list to host parsed info
+            ordersInfo = data['orders']
+            orders = []
+
+            # Iterate over json file into array
+            for orderdetail in ordersInfo:
+                order_id = orderdetail['orderID']
+                product_id = orderdetail['productID']
+                user_id = orderdetail['userID']
+                quantity = orderdetail['quantity']
+
+                # Create an instance of the Products class with the parsed information
+                order = Orders(order_id, product_id, user_id, quantity)
+
+                # Add the order to the list of orders
+                orders.append(order)
+
+            return orders
