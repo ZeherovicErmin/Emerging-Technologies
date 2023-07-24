91719f07fe6b32bc9c50ef0713e0ff7bf9482dea
ZeherovicErmin <93688038+ZeherovicErmin@users.noreply.github.com>
2023-07-24 15:37:22 -0400

    Update userAccount.py
    Laying baseline for Creating user Registration system

--- userAccount.py
+++ userAccount.py
@@ -1,2 +1,21 @@
+from flask import Flask,jsonify,request
+app = Flask(__name__)
+
+
 # An application designed to provide a user account creation/login system
 
+
+@app.route('/user', methods=['GET'])
+
+def rand_joke():
+    if request.method == 'GET':
+    
+
+        
+        # Gathering a variable from our url
+        number = int(request.args.get('num', default=1))
+
+
+        
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
