f1a1442e6097ee63d98c288d2efb67c4761d28d9
ZeherovicErmin <93688038+ZeherovicErmin@users.noreply.github.com>
2023-07-26 19:57:34 -0400

    Attempting to Store to JSON FILE
    Issue with creating a second user, after refreshing the page.

--- userAccount.py
+++ userAccount.py
@@ -7,12 +7,14 @@ app = Flask(__name__)
 # An Array to store any created users information 
 userList = []
 
-
 # Creating a json file to store the data from our array
 storage = "userList.json"
 
-with open(storage, "w") as json_file:
-    json.dump(userList, json_file)
+# Creating a function to load userlist to prevent duplicate users created
+
+def loadUserList():
+    with open(storage, "r") as json_file:
+        return json.load(json_file)
 
 # We will use the /user with methods of POST and GET to either get user information or display it respectively
 @app.route('/user', methods=['GET', 'POST'])
@@ -20,7 +22,7 @@ def user():
     if request.method == 'GET':
         return jsonify(userList), 200
     elif request.method == 'POST':
-        data = request.form
+        data = request.get_json
         if 'username' in data and 'password' in data:
             # Check if the username is already taken
             if any(user['username'] == data['username'] for user in userList):
@@ -33,10 +35,14 @@ def user():
                 'Password': data['password']
             }
             userList.append(new_user)
+            
+            with open(storage, "a") as json_file:
+                json.dump(new_user, json_file)
+
             return jsonify({'message': 'User created successfully'}), 201
         else:
             return jsonify({'message': 'Username and password are required'}), 400
-
+        
 
 @app.route('/', methods=['GET'])
 def index():
