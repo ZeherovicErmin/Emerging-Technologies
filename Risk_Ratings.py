d203b75094bbad8a30d8ba22cc9965651d827980
PriyeshPandya <priyesp@gmail.com>
2023-07-26 19:26:03 -0400

    Created Risk_Ratings.py.

---
+++ Risk_Ratings.py
@@ -0,0 +1,13 @@
+import random
+from flask import Flask, request, jsonify, make_response, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/Risk_Ratings")
+def booking():
+    return render_template('Risk_Ratings.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)
