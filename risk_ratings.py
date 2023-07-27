b4c76ac3472567ce25f5e3dd4a5782421539f8c9
PriyeshPandya <priyesp@gmail.com>
2023-07-26 20:20:32 -0400

    Updated Risk_Ratings.html and updated Risk_Ratings.py

--- risk_ratings.py
+++ risk_ratings.py
@@ -1,14 +1,13 @@
 import random
-import json
 from flask import Flask, request, jsonify, make_response, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/Risk_Ratings")
+@app.route("/Ratings", methods=['GET'])
 def ratings():
     return render_template('Risk_Ratings.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
