b845989007d51c5f2af5228b48ea9bc41a061298
ZeherovicErmin <93688038+ZeherovicErmin@users.noreply.github.com>
2023-07-27 20:52:37 -0400

    paymentProcessor_v0.2
    Implemented userList json and appending payment information to list

--- paymentProcessor.py
+++ paymentProcessor.py
@@ -4,6 +4,22 @@ app = Flask(__name__)
 
 # A simple payment processing implementation 
 
+
+# An Array to store any created users information 
+userList = []
+
+# Creating a variable to store the json filepath
+storage = "userList.json"
+
+# Creating a userList.json file if it doesn't exist already
+if not os.path.exists(storage):
+    with open(storage, "w") as json_file:
+        json.dump([],json_file)
+            
+# Populating our array with users already created
+with open(storage, "r") as json_file:
+    userList = json.load(json_file)
+    
 @app.route('/user/payment', methods=['POST'])
 
 def payment():
@@ -19,8 +35,14 @@ def payment():
          'Expdate' : data['expdate'],
          'CCV'     : data['ccv']
             }
+    userList.append(user_payment)
+    
+    with open(storage, "w") as json_file:
+                json.dump(userList, json_file)
+                
     # Return statement if username/password is incorrect       
     return jsonify({'message': 'Your payment method has been saved succesfully.'},401)     
+    
     
     
          
