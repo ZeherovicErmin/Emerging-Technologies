8809ed2055d9d23ed427836064f46d8794708d06
PriyeshPandya <priyesp@gmail.com>
2023-07-30 14:11:00 -0400

    Updated emergingFront.html and added new emergingFront.py.

--- emergingFront.py
+++ emergingFront.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/Home", methods=['GET'])
 def ratings():
-    return render_template('emergingFront.html')
+    return render_template('emergingfront.html')
 
 
 if __name__ == '__main__':
