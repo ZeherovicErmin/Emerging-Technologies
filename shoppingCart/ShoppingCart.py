25dbacb03701a5c8f9d1fe3fd824648e4f194cd0
Hasan-Ahmad8 <89357738+Hasan-Ahmad8@users.noreply.github.com>
2023-07-31 21:04:02 -0400

    Add files via upload

--- shoppingCart/ShoppingCart.py
+++ shoppingCart/ShoppingCart.py
@@ -1,15 +1,20 @@
 from flask import Flask, render_template, request, redirect, jsonify
 import requests
 import json
-import urllib.request
-app = Flask(__name__)
+import random
+import string
+import os
+app = Flask(__name__, template_folder='../templates')
 
+#Global
+shoppingCart = []
+user_id = None
 # Add an item class
 class Item:
-    def __init__(self, name, price, ID):
+    def __init__(self, name, price, productID):
         self.name = name
         self.price = price
-        self.ID = ID
+        self.productID = productID
 
     def get_name(self):
         return self.name
@@ -17,10 +22,15 @@ class Item:
     def get_price(self):
         return self.price
     
-    def get_id(self):
-        return self.ID
+    def get_productID(self):
+        return self.productID
 
-shoppingCart = []
+@app.route('/welcome/<userID>', methods=["GET","POST"])
+def welcome(userID):
+    global user_id
+    user_id = userID
+    return render_template('welcomeShoppingcart.html')
+
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
@@ -37,18 +47,18 @@ def main():
             price = totalPrice()
             return render_template('index.html', message=f"Total: ${price:.2f}")
         elif action == '4':
-            return checkout()
+            return checkout(user_id)
         else:
             return render_template('index.html', message="Invalid action")
     else:
         return render_template('index.html', message="")
 
 # Display addItem.html
-@app.route('/shoppingcart', methods=['GET'])
+@app.route('/shoppingcart/', methods=['GET'])
 def showAddItemPage():
     return render_template('addItem.html')
 # Add a product to the shopping cart. If the item already exists, increment.
-@app.route('/shoppingcart/<UserID>', methods=['POST'])
+@app.route('/shoppingcart/', methods=['POST'])
 def addItem():
     global shoppingCart
     chosenItem = request.form.get('chosenItem')
@@ -83,37 +93,19 @@ def addToCart(item):
 
     # Check if the item already exists in the shopping cart
     for i, (cart_item, quantity) in enumerate(shoppingCart):
-        if cart_item.ID == item_instance.ID:
+        if cart_item.productID == item_instance.productID:
             shoppingCart[i] = (cart_item, quantity + 1)
             break
     else:
         shoppingCart.append((item_instance, 1))
     
-
-@app.route('/checkout', methods=['GET', 'POST'])
-def checkout():
-    global shoppingCart
-
-    items_list = []
-    for item, quantity in shoppingCart:
-        item_info = {
-            'name': item.get_name(),
-            'price': item.get_price(),
-            'quantity': quantity
-        }
-        items_list.append(item_info)
-
-    return jsonify(items_list)
-
-
-
 # Display removeItem.html
-@app.route('/removeshoppingcart', methods=['GET'])
+@app.route('/removeshoppingcart/', methods=['GET'])
 def showRemoveItemPage():
     return render_template('removeItem.html')
 
 # Remove a product from the shopping cart.
-@app.route('/removeshoppingcart', methods=['POST'])
+@app.route('/removeshoppingcart/', methods=['POST'])
 def removeItem():
     global shoppingCart
     chosenItem = request.form.get('chosenItem')
@@ -127,7 +119,7 @@ def removeItem():
 def removeFromList(chosenItem):
     global shoppingCart
     for i, (item, quantity) in enumerate(shoppingCart):
-        if item.ID == chosenItem:
+        if item.productID == chosenItem:
             if quantity > 1:
                 shoppingCart[i] = (item, quantity - 1)
             else:
@@ -140,6 +132,41 @@ def totalPrice():
     for item, quantity in shoppingCart:
         total += item.get_price() * quantity
     return total
+
+@app.route('/checkout/', methods=['GET', 'POST'])
+def checkout(user_id):
+    global shoppingCart
+
+    items_list = []
+    for item, quantity in shoppingCart:
+        item_info = {
+            'orderID': generateOrderID(),
+            'productID': item.get_productID(),
+            'userID': user_id,
+            'quantity': quantity
+        }
+        items_list.append(item_info)
+
+    if user_id is not None:
+        filename = os.path.join(os.path.dirname(__file__), "static", "shoppingCart.json")
+        if os.path.exists(filename):
+            with open(filename, 'r') as f:
+                existing_data = json.load(f)
+            existing_data.append(items_list)
+        else:
+            existing_data = [items_list]
+
+        with open(filename, 'w') as f:
+            json.dump(existing_data, f)
+
+    return jsonify(items_list)
+
+
+def generateOrderID():
+    characters = string.ascii_letters + string.digits
+    orderID = ''.join(random.choice(characters) for _ in range(6))
+
+    return orderID
 
 #NOTE: for this, have the products.py application running on port 80, if needed you can change the productsUrl to whatever else link.
 #If you do change the url, make sure the fields match such as productName, productId, and productPrice.
@@ -161,4 +188,4 @@ def get_products_from_products_microservice(productName):
         return "Error connecting to the microservice: " + str(e)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=12000)
+    app.run(host='0.0.0.0', port=10)
